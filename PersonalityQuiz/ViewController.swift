1d1469c3935071b9ab138dd515652610602116c8
Dallin Smuin <dallinsmuin@dallins-macbook-pro.local>
2018-10-11 09:36:56 -0600

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Dallin Smuin on 10/11/18.
+//  Copyright © 2018 Dallin Smuin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
